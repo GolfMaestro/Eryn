22f2cfed9af318619efad7d8f7df68fcbc0351cb
GolfMaestro <vlad2004e@gmail.com>
2023-09-05 19:02:45 +0300

    Added input and output

---
+++ Eryn/Eryn/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	char user_input;
+
+	cout << "Hello world" << '\n' << "Input char: ";
+
+	cin >> user_input;
+
+	// cout << user_input;
+}
